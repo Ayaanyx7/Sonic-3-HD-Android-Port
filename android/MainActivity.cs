483a128173ab78acaf3967fc6c068f0214b8ec5c
Ayaanyx7 <ayaanyx7@gmail.com>
2026-09-21 21:04:08 +0100

    Add MainActivity for Sonic 3 HD Android app

---
+++ android/MainActivity.cs
@@ -0,0 +1,27 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.View;
+using Microsoft.Xna.Framework;
+
+[Activity(Label = "Sonic 3 HD", 
+          MainLauncher = true, 
+          Theme = "@android:style/Theme.NoTitleBar.Fullscreen", 
+          AlwaysRetainTaskState = true, 
+          LaunchMode = LaunchMode.SingleInstance, 
+          ScreenOrientation = ScreenOrientation.Landscape, 
+          ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
+public class MainActivity : AndroidGameActivity
+{
+    protected override void OnCreate(Bundle bundle)
+    {
+        base.OnCreate(bundle);
+        
+        // Bypasses the Windows Forms startup object Class1 launcher entirely.
+        // Launches the primary Game instance engine class loop directly.
+        var gameInstance = new global::Sonic_3_HD.Class0(); 
+        
+        SetContentView((View)gameInstance.Services.GetService(typeof(View)));
+        gameInstance.Run();
+    }
+}
